ea94a32ee8b46440e4bd476fbbb75e2338598e51
Kelly Smith <17773027+keligula@users.noreply.github.com>
2018-06-08 13:18:25 -0700

    fixed eslint error

--- client/src/components/CreatePractice.js
+++ client/src/components/CreatePractice.js
@@ -2,76 +2,79 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class CreatePractice extends Component {
-    constructor() {
-        super();
-        this.state = {
-            date: '',
-            instructor: '',
-            classType: '',
-            rating: ''
-        };
-    }
-
-    onChange = e => {
-        const state = this.state;
-        state[e.target.name] = e.target.value;
-        this.setState(state);
+  constructor() {
+    super();
+    this.state = {
+      date: '',
+      instructor: '',
+      classType: '',
+      rating: ''
     };
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
+  onChange = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
 
-        const { date, instructor, classType, rating } = this.state;
+  onSubmit = e => {
+    e.preventDefault();
 
-        axios.post('/api/practices', this.state).then(result => {
-            console.log(result);
-        }).catch(function(err) {
-          console.log(err);
-        });
-    };
+    const { date, instructor, classType, rating } = this.state;
 
-    render() {
-        const { date, instructor, classType, rating } = this.state;
-        return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <label>Date</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="date"
-                        value={date}
-                        onChange={this.onChange}
-                    />
-                    <label>Instructor</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="instructor"
-                        value={instructor}
-                        onChange={this.onChange}
-                    />
-                    <label>Class Type</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="classType"
-                        value={classType}
-                        onChange={this.onChange}
-                    />
-                    <label>Rating</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="rating"
-                        value={rating}
-                        onChange={this.onChange}
-                    />
-                    <button type="submit">ENTER</button>
-                </form>
-            </div>
-        );
-    }
+    axios
+      .post('/api/practices', this.state)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { date, instructor, classType, rating } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Date</label>
+          <input
+            type="text"
+            className="form-control"
+            name="date"
+            value={date}
+            onChange={this.onChange}
+          />
+          <label>Instructor</label>
+          <input
+            type="text"
+            className="form-control"
+            name="instructor"
+            value={instructor}
+            onChange={this.onChange}
+          />
+          <label>Class Type</label>
+          <input
+            type="text"
+            className="form-control"
+            name="classType"
+            value={classType}
+            onChange={this.onChange}
+          />
+          <label>Rating</label>
+          <input
+            type="text"
+            className="form-control"
+            name="rating"
+            value={rating}
+            onChange={this.onChange}
+          />
+          <button type="submit">ENTER</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreatePractice;
