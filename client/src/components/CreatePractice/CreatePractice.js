fc6115b4c294f9263e32e70496392141a0091352
Kelly Smith <17773027+keligula@users.noreply.github.com>
2018-08-22 17:57:37 -0700

    styled create practice form

--- client/src/components/CreatePractice/CreatePractice.js
+++ client/src/components/CreatePractice/CreatePractice.js
@@ -10,12 +10,16 @@ const styles = {
   container: {
     display: 'flex',
     flexWrap: 'wrap',
-    alignItems: 'center',
     width: '100%',
-    fullWidth: true
+    alignItems: 'center',
+    fullWidth: true,
+    paddingTop: 20,
+    paddingBottom: 20,
+    justifyContent: 'center'
   },
   textField: {
-    width: 400
+    width: 400,
+    paddingRight: 15
   }
 };
 
@@ -54,40 +58,44 @@ class CreatePractice extends Component {
     const { classes } = this.props;
 
     return (
-      <form className={classes.container} onSubmit={this.onSubmit}>
-        <TextField
-          name="date"
-          label="Date"
-          className={classes.textField}
-          value={date}
-          onChange={this.handleChange}
-          type="text"
-        />
-        <TextField
-          name="instructor"
-          label="Instructor"
-          className={classes.textField}
-          value={instructor}
-          onChange={this.handleChange}
-        />
-        <TextField
-          name="classType"
-          label="Class Type"
-          className={classes.textField}
-          value={classType}
-          onChange={this.handleChange}
-        />
-        <TextField
-          name="rating"
-          label="Rating"
-          className={classes.textField}
-          value={rating}
-          onChange={this.handleChange}
-        />
-        <Button variant="contained" color="primary" type="submit">
-          LOG PRACTICE
-        </Button>
-      </form>
+      <div>
+        <form className={classes.container} onSubmit={this.onSubmit}>
+          <TextField
+            name="date"
+            label="Date"
+            className={classes.textField}
+            value={date}
+            onChange={this.handleChange}
+            type="text"
+          />
+          <TextField
+            name="instructor"
+            label="Instructor"
+            className={classes.textField}
+            value={instructor}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="classType"
+            label="Class Type"
+            className={classes.textField}
+            value={classType}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="rating"
+            label="Rating"
+            className={classes.textField}
+            value={rating}
+            onChange={this.handleChange}
+          />
+          <div className={classes.container}>
+            <Button variant="contained" color="primary" type="submit">
+              LOG PRACTICE
+            </Button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
