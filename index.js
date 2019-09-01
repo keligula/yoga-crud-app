48eccf2b1ffea84c0e416a97702e3d0d606b9de5
Kelly Smith <17773027+keligula@users.noreply.github.com>
2019-09-01 11:05:30 -0700

    fixing mongoose deprecation errors

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const keys = require('./config/keys');
 require('./models/Practice');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
+mongoose.connect(
+    keys.mongoURI,
+    { useNewUrlParser: true }
+);
+mongoose.createConnection(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
@@ -14,16 +18,16 @@ app.use(bodyParser.json());
 require('./routes/practiceRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file
-  app.use(express.static('client/build'));
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+    app.use(express.static('client/build'));
 
-  // Express will serve up the index.html file
-  // if it does not recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    // Express will serve up the index.html file
+    // if it does not recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5001;
